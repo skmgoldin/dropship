25d749650be097ce8ce49a36a60f6f2e80c0cb55
Mike Goldin <skmgoldin@gmail.com>
2015-04-02 20:23:17 -0400

    Default install option for homebrew

--- osxbootstrap.sh
+++ osxbootstrap.sh
@@ -1,8 +1,12 @@
 # Install Homebrew deps
 xcode-select --install >> .droplog 2>&1
 
-# Install Homebrew
+# Install Homebrew (Default)
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Install Homebrew (Custom)
 mkdir ~/dev >> .droplog 2>&1
 cd ~/dev
-mkdir homebrew && curl -L https://github.com/Homebrew/homebrew/tarball/master |  tar xz --strip 1 -C homebrew 
+mkdir homebrew && curl -L https://github.com/Homebrew/homebrew/tarball/master | tar xz --strip 1 -C homebrew 
+
 
