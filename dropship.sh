662bbd795c41e42455b982dcede07f2208e2fa2b
Mike Goldin <skmgoldin@gmail.com>
2015-04-02 21:26:05 -0400

    Checks if a previous install failed before trying again.

--- dropship.sh
+++ dropship.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
+# Check if a droplog file exists, indicating a previous install attempt failed.
+if [ -e .droplog ]; then
+  echo A file called .droplog already exists in this directory. If you need \
+    the information therein for debugging, save it somewhere else and delete \
+    the file before running dropship again.
+  exit
+fi
+
 # Detect OS
 OS=$(./osdetect.sh)
 echo "Operating system: $OS"
