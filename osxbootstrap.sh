7efe5bfbc379dc311be188bfafbd60f395afce85
Mike Goldin <skmgoldin@gmail.com>
2015-04-02 20:33:54 -0400

    Check if Homebrew is already installed before attempting install.

--- osxbootstrap.sh
+++ osxbootstrap.sh
@@ -1,6 +1,11 @@
 # Install Homebrew deps
 xcode-select --install >> .droplog 2>&1
 
+# If Homebrew is already installed and in the PATH, exit.
+if command -v brew; then
+  exit;
+fi
+
 # Install Homebrew (Default)
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
